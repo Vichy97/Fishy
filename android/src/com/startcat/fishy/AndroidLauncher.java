2a2ff8f8f7316e61f1b3ccd57fb32e489006290a
Vincent Williams <vincentwilliams97@gmail.com>
2016-09-09 16:29:27 -0500

    implemented admob banner

--- android/src/com/startcat/fishy/AndroidLauncher.java
+++ android/src/com/startcat/fishy/AndroidLauncher.java
@@ -1,16 +1,101 @@
 package com.startcat.fishy;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.starcat.fishy.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
+
+	private static final String AD_UNIT_ID = "ca-app-pub-6916351754834612/3101802628";
+	protected AdView adView;
+	protected View gameView;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+		RelativeLayout layout = new RelativeLayout(this);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+		layout.setLayoutParams(params);
+
+		AdView admobView = createAdView();
+		layout.addView(admobView);
+		View gameView = createGameView(config);
+		layout.addView(gameView);
+
+		setContentView(layout);
+		startAdvertising(admobView);
+	}
+
+	private AdView createAdView() {
+		adView = new AdView(this);
+		adView.setAdSize(AdSize.SMART_BANNER);
+		adView.setAdUnitId(AD_UNIT_ID);
+		adView.setId(R.id.adViewId);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+		params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+		adView.setLayoutParams(params);
+		adView.setBackgroundColor(Color.BLACK);
+		return adView;
+	}
+
+	private View createGameView(AndroidApplicationConfiguration cfg) {
+		gameView = initializeForView(new MyGdxGame(), cfg);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+		params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+		params.addRule(RelativeLayout.ABOVE, adView.getId());
+		gameView.setLayoutParams(params);
+		return gameView;
+	}
+
+	private void startAdvertising(AdView adView) {
+		AdRequest adRequest = new AdRequest.Builder().build();
+		adView.loadAd(adRequest);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (adView != null) adView.resume();
+	}
+
+	@Override
+	public void onPause() {
+		if (adView != null) adView.pause();
+		super.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+		if (adView != null) adView.destroy();
+		super.onDestroy();
+	}
+
+	@Override
+	public void onBackPressed() {
+		Gdx.app.exit();
 	}
 }
